3514265f8fc585a04a714eca967a7b32c7aed522
Yutaro M <yutaroapp@gmail.com>
2019-06-16 21:52:40 +0900

    NCMBObjectの取得とListViewへの表示実装

--- app/src/main/java/com/example/yutaroapp/camemode/MainActivity.java
+++ app/src/main/java/com/example/yutaroapp/camemode/MainActivity.java
@@ -34,98 +34,64 @@ public class MainActivity extends AppCompatActivity {
   }
   FABState mFABState = FABState.CLOSE;
 
+  // UserInfoDataクラスのデータを格納するListを作成
+  List<NCMBObject> userInfoDataList = new ArrayList<NCMBObject>();
+
+  // UserInfoDataクラスのデータを取得するクエリを作成
+  NCMBQuery<NCMBObject> query = new NCMBQuery<>("UserInfoData");
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     NCMB.initialize(this.getApplicationContext(),"7d6991b6ccf72cfbaf788b0f1315d58796c46f791165a8cb690238c217d2a6a7"
             ,"8c04c78a810b705ef1b8fd6f832f74f6edbe0d99444c95bc1e6fe115a730def7");
-    
-    // リスト要素を設定
-    ListView UserListView = (ListView)findViewById(R.id.user_info_list);
-    ArrayList<UserListItem> userListItems = new ArrayList<>();
-
-    Resources res = getResources();
-    for (int i = 0; i < 15; i++) {
-      Bitmap bmp = BitmapFactory.decodeResource(res, R.mipmap.ic_gohan);
-      UserListItem userItem = new UserListItem(bmp, "ねこにゃん" + String.valueOf(i+1));
-      userListItems.add(userItem);
-    }
-
-    // リスト表示
-    UserListAdapter adapter = new UserListAdapter(this, R.layout.user_info_list_item, userListItems);
-    UserListView.setAdapter(adapter);
-
-    // UserInfoDataクラスのデータを格納するListを作成
-    final List<NCMBObject> userInfoDataList = new ArrayList<NCMBObject>();
-
-    // UserInfoDataクラスのデータを取得するクエリを作成
-    NCMBQuery<NCMBObject> query = new NCMBQuery<>("UserInfoData");
-
-    // updateDateフィールドの降順（新しい順）で取得する
-    query.addOrderByDescending("updateDate");
-    query.setLimit(15);
-    query.findInBackground(new FindCallback<NCMBObject>() {
-      @Override
-      public void done(List<NCMBObject> list, NCMBException e) {
-        if (e != null) {
-          // エラー時
-          Toast.makeText(getApplicationContext(), "データ取得エラー", Toast.LENGTH_SHORT).show();
-        } else {
-          // 成功時
-          // UserInfoDataList の確認
-          Toast.makeText(getApplicationContext(), "データ取得成功", Toast.LENGTH_SHORT).show();
-          for (NCMBObject obj : list) {
-            Log.d("MainActivity", "userInfoDataList DispName: " + obj.getString("DisplayName"));
-            userInfoDataList.add(obj);
-          }
-        }
-      }
-    });
-
-    new Handler().postDelayed(new Runnable() {
-      @Override
-      public void run() {
-//    出力用リストへの格納確認
-        for (NCMBObject obj : userInfoDataList) {
-          Log.d("2MainActivity", "userInfoDataList DispName: " + obj.getString("DisplayName"));
-        }
-      }
-    }, 2000);
 
     // FAB処理
     FloatingActionButton fabMenuButton = (FloatingActionButton) findViewById(R.id.fab);
     fabMenuButton.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        int iconWhile = 350;
-        if (mFABState == mFABState.CLOSE) {
-          fabOpen(iconWhile);
-        } else {
-          fabClose();
+        @Override
+        public void onClick(View view) {
+            int iconWhile = 350;
+            if (mFABState == mFABState.CLOSE) {
+                fabOpen(iconWhile);
+            } else {
+                fabClose();
+            }
         }
-      }
     });
 
     FloatingActionButton fabAddButton = (FloatingActionButton) findViewById(R.id.fab_add);
     fabAddButton.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        fabClose();
-        Intent intent = new Intent(getApplication(), AddActivity.class);
-        startActivity(intent);
-      }
+        @Override
+        public void onClick(View v) {
+            fabClose();
+            Intent intent = new Intent(getApplication(), AddActivity.class);
+            startActivity(intent);
+        }
     });
 
     FloatingActionButton fabSearchButton = (FloatingActionButton) findViewById(R.id.fab_search);
     fabSearchButton.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        fabClose();
-        Intent intent = new Intent(getApplication(), SearchActivity.class);
-        startActivity(intent);
-      }
+        @Override
+        public void onClick(View v) {
+            fabClose();
+            Intent intent = new Intent(getApplication(), SearchActivity.class);
+            startActivity(intent);
+        }
     });
+
+    // updateDateフィールドの降順（新しい順）で取得し、ユーザ情報をListViewに出力する
+    query.addOrderByDescending("updateDate");
+    query.setLimit(15);
+    displayListView(query, userInfoDataList);
+  }
+
+  // onRestartは、Activityが再度呼び出された時に実行するライフサイクルメソッド
+  protected void onRestart() {
+      super.onRestart();
+      query.addOrderByDescending("updateDate");
+      query.setLimit(15);
+      displayListView(query, userInfoDataList);
   }
 
   private void fabOpen(int iconWhile) {
@@ -175,5 +141,41 @@ public class MainActivity extends AppCompatActivity {
 
     mFABState = FABState.CLOSE;
     mFabBackground.animate().alpha(0f).setDuration(200);
+  }
+  private void displayListView(NCMBQuery query, final List<NCMBObject> userInfoDataList) {
+      query.findInBackground(new FindCallback<NCMBObject>() {
+          @Override
+          public void done(List<NCMBObject> list, NCMBException e) {
+              if (e != null) {
+                  // エラー時
+                  Toast.makeText(getApplicationContext(), "データ取得エラー", Toast.LENGTH_SHORT).show();
+              } else {
+                  // 成功時
+                  // UserInfoDataList の確認
+                  Toast.makeText(getApplicationContext(), "データ取得成功", Toast.LENGTH_SHORT).show();
+
+                  for (NCMBObject obj : list) {
+                      Log.d("MainActivity", "userInfoDataList DispName: " + obj.getString("DisplayName"));
+                      userInfoDataList.add(obj);
+                  }
+                  display();
+              }
+          }
+      });
+  }
+
+  private void display() {
+      Resources res = getResources();
+      ListView UserListView = (ListView)findViewById(R.id.user_info_list);
+      ArrayList<UserListItem> userListItems = new ArrayList<>();
+
+      for (NCMBObject obj : userInfoDataList) {
+          Bitmap bmp = BitmapFactory.decodeResource(res, R.mipmap.ic_gohan);
+          UserListItem userItem = new UserListItem(bmp, obj.getString("DisplayName"));
+          userListItems.add(userItem);
+      }
+
+      UserListAdapter adapter = new UserListAdapter(this, R.layout.user_info_list_item, userListItems);
+      UserListView.setAdapter(adapter);
   }
 }

ad173cfcba9950ffcf2f28a7a44d337e5e1b3823
Yutaro M <yutaroapp@gmail.com>
2019-07-16 00:44:13 +0900

    MainActivityとMainLayoutを分離。 ※メニューボタンを押すと落ちる。。。

--- app/src/main/java/com/example/yutaroapp/camemode/MainActivity.java
+++ app/src/main/java/com/example/yutaroapp/camemode/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.yutaroapp.camemode;
 
 import android.animation.ObjectAnimator;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
@@ -12,10 +15,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.nifcloud.mbaas.core.FindCallback;
@@ -29,67 +34,54 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    /* MainLayout */
+    private MainLayout mMainLayout;
+
+    /* onActivityResultで用いるREQUEST_CODE */
     private static final int REQUEST_CODE = 1;
 
-    // FABの状態管理用列挙体
+    /* FABの状態管理用列挙体 */
     enum FABState {
         OPEN,
         CLOSE
     }
+    static FABState mFABState = FABState.CLOSE;
 
-    FABState mFABState = FABState.CLOSE;
-
-    // UserInfoDataクラスのデータを格納するListを作成
+    /* UserInfoDataクラスのデータを格納するList */
     List<NCMBObject> userInfoDataList = new ArrayList<NCMBObject>();
 
-    // UserInfoDataクラスのデータを取得するクエリを作成
+    /* UserInfoDataクラスのデータを取得するクエリ */
     NCMBQuery<NCMBObject> query = new NCMBQuery<>("UserInfoData");
+
+    @SuppressLint("ResourceType")
+    private void setupViews() {
+        mMainLayout = new MainLayout(getApplicationContext());
+//        mMainLayout = (MainLayout) findViewById(R.layout.activity_main);
+        mMainLayout.setUpViews(getWindow().getDecorView());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupViews();
+
         NCMB.initialize(this.getApplicationContext(), "7d6991b6ccf72cfbaf788b0f1315d58796c46f791165a8cb690238c217d2a6a7"
                 , "8c04c78a810b705ef1b8fd6f832f74f6edbe0d99444c95bc1e6fe115a730def7");
 
-        // FAB処理
-        FloatingActionButton fabMenuButton = (FloatingActionButton) findViewById(R.id.fab);
-        fabMenuButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int iconWhile = 350;
-                if (mFABState == mFABState.CLOSE) {
-                    fabOpen(iconWhile);
-                } else {
-                    fabClose();
-                }
-            }
-        });
-
-        FloatingActionButton fabAddButton = (FloatingActionButton) findViewById(R.id.fab_add);
-        fabAddButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                fabClose();
-                Intent intent = new Intent(getApplication(), AddActivity.class);
-                startActivityForResult(intent, REQUEST_CODE);
-            }
-        });
-
-        FloatingActionButton fabSearchButton = (FloatingActionButton) findViewById(R.id.fab_search);
-        fabSearchButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                fabClose();
-                Intent intent = new Intent(getApplication(), SearchActivity.class);
-                startActivityForResult(intent, REQUEST_CODE);
-            }
-        });
-
-        // updateDateフィールドの降順（新しい順）で取得し、ユーザ情報をListViewに出力する
+        // updateDateフィールドの新しい順にデータ取得し、ListViewに出力
         query.addOrderByDescending("updateDate");
         query.setLimit(15);
         displayListView(query, userInfoDataList);
+    }
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        setupViews();
+//    }
+
+    public static void startActivityforResult(Activity activity, Intent intent) {
+        activity.startActivityForResult(intent, REQUEST_CODE);
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -104,57 +96,6 @@ public class MainActivity extends AppCompatActivity {
             default:
                 break;
         }
-
-    }
-
-    private void fabOpen(int iconWhile) {
-        LinearLayout mFabSearchLayout = findViewById(R.id.fab_search_layout);
-        LinearLayout mFabAddLayout = findViewById(R.id.fab_add_layout);
-        FloatingActionButton mFab = findViewById(R.id.fab);
-        View mFabBackground = findViewById(R.id.fab_background);
-
-        mFabSearchLayout.setVisibility(View.VISIBLE);
-        ObjectAnimator animator = ObjectAnimator.ofFloat(mFabSearchLayout, "translationY", -iconWhile);
-        animator.setDuration(250);
-        animator.start();
-
-        mFabAddLayout.setVisibility(View.VISIBLE);
-        animator = ObjectAnimator.ofFloat(mFabAddLayout, "translationY", -iconWhile * 2);
-        animator.setDuration(250);
-        animator.start();
-
-        mFab.setImageResource(R.drawable.ic_clear_button);
-        animator = ObjectAnimator.ofFloat(mFab, "rotation", 270);
-        animator.setDuration(250);
-        animator.start();
-
-        mFABState = FABState.OPEN;
-        mFabBackground.animate().alpha(0.8f).setDuration(300);
-    }
-
-    private void fabClose() {
-        LinearLayout mFabSearchLayout = findViewById(R.id.fab_search_layout);
-        LinearLayout mFabAddLayout = findViewById(R.id.fab_add_layout);
-        FloatingActionButton mFab = findViewById(R.id.fab);
-        View mFabBackground = findViewById(R.id.fab_background);
-
-        ObjectAnimator animator = ObjectAnimator.ofFloat(mFabSearchLayout, "translationY", 0);
-        animator.setDuration(200);
-        animator.start();
-        mFabSearchLayout.setVisibility(View.GONE);
-
-        animator = ObjectAnimator.ofFloat(mFabAddLayout, "translationY", 0);
-        animator.setDuration(200);
-        animator.start();
-        mFabAddLayout.setVisibility(View.GONE);
-
-        mFab.setImageResource(R.drawable.ic_menu_button);
-        animator = ObjectAnimator.ofFloat(mFab, "rotation", 0);
-        animator.setDuration(200);
-        animator.start();
-
-        mFABState = FABState.CLOSE;
-        mFabBackground.animate().alpha(0f).setDuration(200);
     }
 
     private void displayListView(NCMBQuery query, final List<NCMBObject> userInfoDataList) {
---
+++ app/src/main/java/com/example/yutaroapp/camemode/MainLayout.java
@@ -0,0 +1,125 @@
+package com.example.yutaroapp.camemode;
+
+import android.animation.ObjectAnimator;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import static com.example.yutaroapp.camemode.MainActivity.*;
+
+public class MainLayout extends RelativeLayout {
+
+//    FloatingActionButton fabMenuButton = (FloatingActionButton) findViewById(R.id.fab);
+//    FloatingActionButton fabAddButton = (FloatingActionButton) findViewById(R.id.fab_add);
+//    FloatingActionButton fabSearchButton = (FloatingActionButton) findViewById(R.id.fab_search);
+
+    /** コンテキスト. */
+    private Context mContext;
+
+    /* コンストラクタ（理由もわからずとりあえず3つ設けている） */
+    public MainLayout(Context context) {
+        super(context);
+        mContext = context;
+    }
+    public MainLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mContext = context;
+    }
+    public MainLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        mContext = context;
+    }
+
+    public void setUpViews(View view) {
+        // FAB処理
+        FloatingActionButton fabMenuButton = (FloatingActionButton) view.findViewById(R.id.fab);
+        fabMenuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int iconWhile = 350;
+                if (mFABState == mFABState.CLOSE) {
+                    fabOpen(iconWhile);
+                } else {
+                    fabClose();
+                }
+            }
+        });
+
+        FloatingActionButton fabAddButton = (FloatingActionButton) view.findViewById(R.id.fab_add);
+        fabAddButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fabClose(); // FABを閉じてから遷移
+                Intent intent = new Intent(mContext, AddActivity.class);
+//                mContext.startActivity(intent);
+                MainActivity.startActivityforResult((Activity) mContext.getApplicationContext(), intent);
+            }
+        });
+
+        FloatingActionButton fabSearchButton = (FloatingActionButton) view.findViewById(R.id.fab_search);
+        fabSearchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fabClose(); // FABを閉じてから遷移
+                Intent intent = new Intent(mContext, SearchActivity.class);
+//                mContext.startActivity(intent);
+                MainActivity.startActivityforResult((Activity) mContext.getApplicationContext(), intent);
+            }
+        });
+    }
+
+    private void fabOpen(int iconWhile) {
+        LinearLayout mFabSearchLayout = findViewById(R.id.fab_search_layout);
+        LinearLayout mFabAddLayout = findViewById(R.id.fab_add_layout);
+        FloatingActionButton mFab = findViewById(R.id.fab);
+        View mFabBackground = findViewById(R.id.fab_background);
+
+        mFabSearchLayout.setVisibility(View.VISIBLE);
+        ObjectAnimator animator = ObjectAnimator.ofFloat(mFabSearchLayout, "translationY", -iconWhile);
+        animator.setDuration(250);
+        animator.start();
+
+        mFabAddLayout.setVisibility(View.VISIBLE);
+        animator = ObjectAnimator.ofFloat(mFabAddLayout, "translationY", -iconWhile * 2);
+        animator.setDuration(250);
+        animator.start();
+
+        mFab.setImageResource(R.drawable.ic_clear_button);
+        animator = ObjectAnimator.ofFloat(mFab, "rotation", 270);
+        animator.setDuration(250);
+        animator.start();
+
+        mFABState = MainActivity.FABState.OPEN;
+        mFabBackground.animate().alpha(0.8f).setDuration(300);
+    }
+
+    private void fabClose() {
+        LinearLayout mFabSearchLayout = findViewById(R.id.fab_search_layout);
+        LinearLayout mFabAddLayout = findViewById(R.id.fab_add_layout);
+        FloatingActionButton mFab = findViewById(R.id.fab);
+        View mFabBackground = findViewById(R.id.fab_background);
+
+        ObjectAnimator animator = ObjectAnimator.ofFloat(mFabSearchLayout, "translationY", 0);
+        animator.setDuration(200);
+        animator.start();
+        mFabSearchLayout.setVisibility(View.GONE);
+
+        animator = ObjectAnimator.ofFloat(mFabAddLayout, "translationY", 0);
+        animator.setDuration(200);
+        animator.start();
+        mFabAddLayout.setVisibility(View.GONE);
+
+        mFab.setImageResource(R.drawable.ic_menu_button);
+        animator = ObjectAnimator.ofFloat(mFab, "rotation", 0);
+        animator.setDuration(200);
+        animator.start();
+
+        mFABState = MainActivity.FABState.CLOSE;
+        mFabBackground.animate().alpha(0f).setDuration(200);
+    }
+}

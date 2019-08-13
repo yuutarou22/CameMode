4932f19ff7dd40b6af9b3eeac929f418ba18ba17
YutaroM <yutaroapp@gmail.com>
2019-08-14 08:56:29 +0900

    検索結果のユーザ情報出力実装（途中） 現状として、検索2回目になってデータが表示される。 ライフサイクルとUtilityのデータ登録時に問題あり？

--- app/src/main/java/com/example/yutaroapp/camemode/SearchResultActivity.java
+++ app/src/main/java/com/example/yutaroapp/camemode/SearchResultActivity.java
@@ -1,9 +1,19 @@
 package com.example.yutaroapp.camemode;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+
+import com.nifcloud.mbaas.core.NCMBObject;
+
+import java.util.ArrayList;
 
 public class SearchResultActivity extends AppCompatActivity {
     /* SearchResultLayout */
@@ -19,11 +29,42 @@ public class SearchResultActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_result);
         setupViews();
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
         Intent intent = getIntent();
         Log.d("onCreate", "intent: " + intent.toString());
         for (int i = 0; i<Utility.userInfoDataList.size(); i++) {
             Log.d("onCreate", "getStringArrayList " + Utility.userInfoDataList.get(i));
         }
+        displayListView();
+    }
+
+    /**
+     * 取得したユーザ情報をリストビューに出力する
+     */
+    private void displayListView() {
+        Resources res = getResources();
+        RecyclerView userListView = (RecyclerView) findViewById(R.id.user_info_list);
+        RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        userListView.addItemDecoration(itemDecoration);
+
+        ArrayList<UserListItem> userListItems = new ArrayList<>();
+        // 取得したユーザ情報をもとに、アイテムに格納
+        for (NCMBObject obj : Utility.userInfoDataList) {
+            Bitmap bmp = BitmapFactory.decodeResource(res, R.mipmap.ic_gohan);
+            // ユーザ情報アイテムを生成
+            UserListItem userItem = new UserListItem(bmp, obj.getString("DisplayName"), obj.getString("CategoryRole"));
+            userListItems.add(userItem);
+        }
+
+        UserRecyclerViewAdapter adapter = new UserRecyclerViewAdapter(userListItems);
+        LinearLayoutManager llm = new LinearLayoutManager(getApplicationContext());
+        userListView.setHasFixedSize(true);
+        userListView.setLayoutManager(llm);
+
+        userListView.setAdapter(adapter);
     }
 }

f91079190477095da022cf5d16633354a455b90f
YutaroM <yutaroapp@gmail.com>
2019-11-20 23:50:24 +0900

    0034_初回ページャーの実装 ・ページャ実装 残：初回起動時のみ機能するよう修正、表示する内容の画像指定、レイアウト、リファクタリング

---
+++ app/src/main/java/com/example/yutaroapp/camemode/TutorialPagerFragment/TutorialPagerFragment3.java
@@ -0,0 +1,16 @@
+package com.example.yutaroapp.camemode.TutorialPagerFragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.yutaroapp.camemode.R;
+
+public class TutorialPagerFragment3 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_tutorial_pager_3, container, false);
+    }
+}

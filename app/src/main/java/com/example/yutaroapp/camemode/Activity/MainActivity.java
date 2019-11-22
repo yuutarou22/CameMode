3effa50ba929d9f94bea0ef1ee6b881bf2684537
YutaroM <yutaroapp@gmail.com>
2019-11-23 07:38:59 +0900

    全体で不要な処理を削除

--- app/src/main/java/com/example/yutaroapp/camemode/Activity/MainActivity.java
+++ app/src/main/java/com/example/yutaroapp/camemode/Activity/MainActivity.java
@@ -5,8 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.support.v4.app.AppLaunchChecker;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
--- app/src/main/java/com/example/yutaroapp/camemode/Utility.java
+++ app/src/main/java/com/example/yutaroapp/camemode/Utility.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.util.Log;
 
-import com.example.yutaroapp.camemode.Activity.MainActivity;
 import com.nifcloud.mbaas.core.NCMBObject;
 
 import java.util.ArrayList;
